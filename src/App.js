644186d8bfabc1db8940dd6bd27add8082513616
mtmodesti <marcelomodesti1988@gmail.com>
2022-03-15 12:56:13 -0300

    style: criação de variáveis para as fontes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ const App = () => {
   return (
     <> <GlobalStyle />
       <div className="App"></div>
+      <h1>fonte h1</h1>
+      <h2>fonte h2</h2>
+      <h3>fonte h3</h3>
+      <h4>fonte h4</h4>
+      <h5>fonte h5</h5>
+      <h6>fonte h6</h6>
     </>
   );
 };
