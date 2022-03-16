38326a6b40e1277f15c23447e2f3664448d683b1
mtmodesti <marcelomodesti1988@gmail.com>
2022-03-16 16:01:36 -0300

    feat: criei a landing page stack 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import GlobalStyle from "./styles/global";
-
+import Stack2 from "./components/stack2";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Stack2 />
     </>
   );
 };
