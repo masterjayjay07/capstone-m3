349b7eccb1825b21a457f22512ca5a0e50c27a27
Victor Ramon <victorrgds02@gmail.com>
2022-03-16 15:21:02 -0300

    feat: implementei contexto para autenticaçao

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import GlobalStyle from "./Styles/Global";
+import { Toaster } from "react-hot-toast";
+import GlobalStyle from "./styles/global";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Toaster position="top-right" reverseOrder={false} />
     </>
   );
 };
---
+++ src/provider/User/index.jsx
@@ -0,0 +1,8 @@
+import { createContext, useState } from "react";
+import boraMarcarApi from "../../services/api";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+    
+}
