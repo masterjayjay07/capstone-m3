fc16f315d7d951f4f32ad0dcb5c587a7769eae58
Yorran <yorranjrrigatti@gmail.com>
2022-03-17 09:37:58 -0300

    hotfix: diminui o numero de props passadas pelo tema para o botao

--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 export const Container = styled.button`
   background: ${(props) => props.theme.background};
   border: ${(props) => props.theme.border};
-  font-size: ${(props) => props.theme.fontSize};
-  color: ${(props) => props.theme.color};
-  transition: ${(props) => props.theme.transition};
+  font-size: var(--button-font-size);
+  color: var(--color-linen);
+  transition: 0.3s;
   padding: ${(props) => props.theme.padding};
-  border-radius: ${(props) => props.theme.borderRadius};
-  font-weight: ${(props) => props.theme.fontWeight};
+  border-radius: 10px;
+  font-weight: bold;
 
   :hover {
     background: ${(props) => props.theme.hover.background};
