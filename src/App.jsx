56438f5cd3da8a493877b21464e61e278f67cacd
Victor Ramon <victorrgds02@gmail.com>
2022-03-17 17:38:38 -0300

    hotfix: atualizei estrutura do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import { Toaster } from "react-hot-toast";
 import Routes from "./routes";
 import GlobalStyle from "./styles/global";
-import Input from "./components/input";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-<<<<<<< HEAD
       <Toaster position="top-right" reverseOrder={false} />
       <Routes />
-=======
-      
-      
-      />
->>>>>>> origin/feature/input
     </>
   );
 };
