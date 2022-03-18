f6f6c9b10d42d7d935b19b4d99559f0b8320de85
Anderson Pinto <74440689+andersonstpinto@users.noreply.github.com>
2022-03-18 09:08:10 -0300

    feat: adicionei estilizaçao da dashboard e importei os componentes de listas

--- src/pages/DashboardEvents/index.jsx
+++ src/pages/DashboardEvents/index.jsx
@@ -1,5 +1,49 @@
+import {
+  Container,
+  ContainerHeader,
+  TabButton,
+  EventTitle,
+  Content,
+  MainButton,
+} from "./styles";
+
+import { FaClipboardList, FaUserFriends } from "react-icons/fa";
+
+import { useState } from "react";
+
+import ProductList from "../../components/productList";
+import GuestList from "../../components/guestList";
+import Header from "../../components/header";
+
 const DashboardEvents = () => {
-  return <></>;
+  const [productsTab, setProductsTab] = useState(true);
+  const switchTabs = () => setProductsTab(!productsTab);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <ContainerHeader>
+          <TabButton active={productsTab} onClick={() => switchTabs()}>
+            <p>
+              <FaClipboardList />
+            </p>
+          </TabButton>
+          <TabButton active={!productsTab} onClick={() => switchTabs()}>
+            <p>
+              <FaUserFriends />
+            </p>
+          </TabButton>
+        </ContainerHeader>
+        <EventTitle children={"Casamento do Yorran"} />
+        <Content children={productsTab ? <ProductList /> : <GuestList />} />
+        <MainButton
+          onClick={() => console.log("modal da divisao")}
+          children={"Marcar!"}
+        />
+      </Container>
+    </>
+  );
 };
 
 export default DashboardEvents;
