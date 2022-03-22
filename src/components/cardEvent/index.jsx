09da0fe867262e54d5073a1e8a9af5193c2d68bf
mtmodesti <marcelomodesti1988@gmail.com>
2022-03-22 08:33:07 -0300

    feat: Card de eventos agora informa data e local do evento

--- src/components/cardEvent/index.jsx
+++ src/components/cardEvent/index.jsx
@@ -1,13 +1,19 @@
 import { Container } from "./styles";
 
 const CardEvent = ({ event, ...rest }) => {
-  return <Container {...rest}>{event &&
-    <>
-    <hr />
-    <p>{event.name}       </p>
-    {console.log(event)}
-    </>
-    } </Container>
+  return (
+    <Container {...rest}>
+      {event && (
+        <>
+          <hr />
+          <p>{event.name} </p>
+
+          <span>Quando? {event.location}</span>
+          <span>Onde? {event.date}</span>
+        </>
+      )}{" "}
+    </Container>
+  );
 };
 
 export default CardEvent;
--- src/components/cardEvent/styles.js
+++ src/components/cardEvent/styles.js
@@ -5,7 +5,7 @@ export const Container = styled.div`
   color: var(--color-linen);
   width: 250px;
   height: 150px;
-  border: none; 
+  border: none;
   border-radius: 10px;
   display: flex;
   justify-content: space-evenly;
@@ -15,14 +15,13 @@ export const Container = styled.div`
   transition: 0.5s;
   flex-direction: column;
   cursor: pointer;
-  hr{
-      width: 100px ;
-      height: 5px;
-      background-color: var(--color-stateBlue);
-      border: 1px solid var(--color-darkPurple);
-      border-radius: 50px;
-    }
-
+  hr {
+    width: 100px;
+    height: 5px;
+    background-color: var(--color-stateBlue);
+    border: 1px solid var(--color-darkPurple);
+    border-radius: 50px;
+  }
 
   &:hover {
     box-shadow: 3px 3px 3px black;
@@ -30,25 +29,18 @@ export const Container = styled.div`
     color: var(--color-middleBluePurple);
     border: 1px solid var(--color-darkPurple);
     opacity: 0.9;
-    
 
-    hr{
+    hr {
       border: none;
     }
-    p{
+    p {
       color: var(--color-linen);
       font-weight: 800;
-       opacity: 1;
-
+      opacity: 1;
     }
-    
   }
 
- 
-
-  
   p {
-
     font-size: 1.3rem;
     overflow: hidden;
     text-overflow: ellipsis;
@@ -56,4 +48,11 @@ export const Container = styled.div`
     max-height: 240px;
     white-space: nowrap;
   }
+
+  span{
+    font-size: 0.8rem;
+    border: none;
+  }
+
+
 `;
