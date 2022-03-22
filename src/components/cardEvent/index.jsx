4cca57c0b087b9cefe8f06e18ece5056affdd76c
mtmodesti <marcelomodesti1988@gmail.com>
2022-03-22 08:47:29 -0300

    fix: corrigindo bug do console log no card de evento e removendo uma linha desnecessária

--- src/components/cardEvent/index.jsx
+++ src/components/cardEvent/index.jsx
@@ -1,17 +1,21 @@
 import { Container } from "./styles";
 
 const CardEvent = ({ event, ...rest }) => {
+
+  function dateConverso(date){
+    return date.split('-').reverse().join('/')
+  }
+
   return (
     <Container {...rest}>
       {event && (
         <>
           <hr />
           <p>{event.name} </p>
-
           <span>Quando? {event.location}</span>
-          <span>Onde? {event.date}</span>
+          <span>Onde? {(event.date)}</span>
         </>
-      )}{" "}
+      )}
     </Container>
   );
 };
