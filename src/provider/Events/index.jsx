de0f61209686a95197c38ad3fda328d4f5ced0f1
Anderson Pinto <74440689+andersonstpinto@users.noreply.github.com>
2022-03-23 18:06:46 -0300

    Adicionei toasts de erro e comentarios para a funçao handleLetsMake

--- src/provider/Events/index.jsx
+++ src/provider/Events/index.jsx
@@ -26,10 +26,13 @@ export const EventProvider = ({ children }) => {
     boraMarcarApi
       .get(`/events?userId=${userId}`)
       .then(({ data }) => setUserEvents(data))
-      .catch((error) => console.log(error));
+      .catch(error => {
+        console.log(error);
+        toast.error("Ops! Algo deu errado!");
+      });
   };
 
-  const handleCreateEvent = (data) => {
+  const handleCreateEvent = data => {
     const newEvent = {
       ...data,
       itemsList: [],
@@ -47,7 +50,7 @@ export const EventProvider = ({ children }) => {
       });
   };
 
-  const handleEditEvent = (data) => {
+  const handleEditEvent = data => {
     boraMarcarApi
       .patch(`/events/${activeEvent.id}`, data, {
         headers: { Authorization: `Bearer ${userToken}` },
@@ -57,24 +60,33 @@ export const EventProvider = ({ children }) => {
         setActiveEvent({ ...data });
         toast.success("Evento marcado!");
       })
-      .catch((error) => console.log(error));
+      .catch(error => {
+        console.log(error);
+        toast.error("Ops! Algo deu errado!");
+      });
   };
 
   const handleWhoTakes = () => {
+    // Clone do array de convidados, usado para evitar mutações no array original
     let workGuestsArray = [...guests];
 
     for (let i = 0; i < itemsList.length; i++) {
+      // Sorteia um convidado aleatório com base em sua posição no array de trabalho
       const randomGuest =
         workGuestsArray[Math.floor(Math.random() * workGuestsArray.length)];
 
+      // Acessa o convidado sorteado, sua chave já existe no objeto finalSolution
+      // Push do produto atual no array de produtos do convidado
       finalSolution.guests[randomGuest.name].productList.push(itemsList[i]);
 
-      
+      // Enquanto o array de convidados possuir nomes, vamos filtrando com base no id
+      // Assim podemos garantir que todos os convidados vão receber ao menos um produto
       workGuestsArray = workGuestsArray.filter(guest => {
         return guest.id !== randomGuest.id;
       });
-      
-  
+
+      // Quando não temos mais convidados disponíveis, mas ainda temos produtos para
+      // distribuir, o array de trabalho recebe novamente todos os convidados
       if (workGuestsArray.length === 0) {
         workGuestsArray = [...guests];
       }
@@ -82,11 +94,14 @@ export const EventProvider = ({ children }) => {
   };
 
   const handleCostDivision = () => {
+    // Calcula e cria a propriedade itemCost para cada item contido no array productList
     for (const guest in finalSolution.guests) {
       finalSolution.guests[guest].productList.forEach(
-        (item) => (item.itemCost = Number(item.price) * Number(item.quantity))
+        item => (item.itemCost = Number(item.price) * Number(item.quantity))
       );
 
+      // Calcula e cria a propriedade totalCost para a soma dos custos de todos os
+      // produtos do convidade selecionado
       finalSolution.guests[guest].totalCost =
         finalSolution.guests[guest].productList.reduce(
           (sum, { itemCost }) => (sum += itemCost),
@@ -96,15 +111,19 @@ export const EventProvider = ({ children }) => {
   };
 
   const handleLetsMake = () => {
+    // Calculamos o custo total do evento
     finalSolution.totalPrice = itemsList.reduce(
       (sum, item) => (sum += Number(item.price) * Number(item.quantity)),
       0
     );
 
+    // Calculamos a média de custo por usuário com base no preço total e quantidade
+    // de convidades
     finalSolution.averagePrice = Number(
       (finalSolution.totalPrice / guests.length).toFixed(2)
     );
 
+    // Criamos em finalSolution uma chave para cada convidade
     guests.forEach(({ name }) => {
       finalSolution.guests[name] = {
         totalCost: 0,
@@ -112,7 +131,9 @@ export const EventProvider = ({ children }) => {
       };
     });
 
+    // Definimos as responsabilidades de cada convidade
     handleWhoTakes();
+    // Definimos o custo final para cada usuário, com relação ao produto que recebeu
     handleCostDivision();
     setActiveEvent({
       ...activeEvent,
