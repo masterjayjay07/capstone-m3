3e5d0f81cb22398d573bf783495565420653d598
mtmodesti <marcelomodesti1988@gmail.com>
2022-03-24 14:14:27 -0300

    feat: inserimos animação de scroll na landing page

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -2,11 +2,24 @@ import Footer from "../../components/footerLandingPage";
 import Header from "../../components/header";
 import LpStack1 from "../../components/lpstack1";
 import Stack2 from "../../components/stack2";
-import { motion } from "framer-motion";
+import {
+  motion,
+  useViewportScroll,
+  useSpring,
+  useTransform,
+} from "framer-motion";
+import { useState, useEffect } from "react";
+import { Container } from "./styles";
 
-import ScrollAnimation from "react-animate-on-scroll";
 
 const LandingPage = () => {
+  const [isComplete, setIsComplete] = useState(false);
+  const { scrollYProgress } = useViewportScroll();
+  const yRange = useTransform(scrollYProgress, [0, 0.9], [0, 1]);
+  const pathLength = useSpring(yRange, { stiffness: 400, damping: 90 });
+
+  useEffect(() => yRange.onChange((v) => setIsComplete(v >= 1)), [yRange]);
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -15,7 +28,37 @@ const LandingPage = () => {
       transition={{ duration: 1 }}
     >
       <Header />
+
+
       <main>
+
+      <Container className="progress-icon" viewBox="0 0 60 60">
+        <motion.path
+          fill="none"
+          strokeWidth="5"
+          stroke="white"
+          strokeDasharray="0 1"
+          d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
+          style={{
+            pathLength,
+            rotate: 90,
+            translateX: 5,
+            translateY: 5,
+            scaleX: -1 // Reverse direction of line animation
+          }}
+          />
+        <motion.path
+          fill="none"
+          strokeWidth="5"
+          stroke="white"
+          d="M14,26 L 22,33 L 35,16"
+          initial={false}
+          strokeDasharray="0 1"
+          animate={{ pathLength: isComplete ? 1 : 0 }}
+          />
+      </Container>
+
+
         <LpStack1 />
         <Stack2 />
         <Footer />
